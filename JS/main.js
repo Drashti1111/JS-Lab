68df77b4714f5f6593d8b15cda7b20dc395f1d10
Drashti1111 <dobariyadrashti242@gmail.com>
2024-11-17 20:32:18 -0700

    add greeting in js

--- JS/main.js
+++ JS/main.js
@@ -5,3 +5,21 @@
 const footer = document.getElementById("footer");
 const currentYear = new Date().getFullYear();
 footer.innerHTML = `&copy; Drashti, ${currentYear}`; 
+
+//Greeting message
+const greetingElement = document.querySelector("section div h1");
+const hour = new Date().getHours();
+
+let greeting = "";
+
+if (hour < 12){
+    greeting = "Good Morning!";
+}
+if (hour < 17){
+    greeting = "Good Afternoon!";
+}
+else{
+    greeting = "Good Evening!";
+}
+
+greetingElement.textContent = greeting;
